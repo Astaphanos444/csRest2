1e9747acf8da7058f5c39cacf90d2c1ce556575a
chris <christianpimpo304@hotmail.com>
2025-04-30 09:58:50 -0300

    ,

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<VideoGameDbContext>(options => {
     options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
 });
-
+builder.Services.AddControllers();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
